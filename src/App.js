2a816f731635622d1e259c5e14c84ce253e6af33
Andrew Porter <30642358+DrewDrew22@users.noreply.github.com>
2023-08-17 16:03:30 +0000

    Intial commit of repository, created components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import './App.css';
+import "./App.css";
+import CustomizedAccordions from "./components/Sandwich";
+
+const title = "Sandwich";
+const description =
+  "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <CustomizedAccordions title={title} description={description} />
       </header>
     </div>
   );
