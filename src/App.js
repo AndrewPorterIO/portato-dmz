8cd890d4d12b861356d7b79d3a2d037d45e043f6
Andrew Porter <30642358+DrewDrew22@users.noreply.github.com>
2023-08-18 14:09:48 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
 import React from "react";
 import NestedAccordion from "./components/NestedAccordion";
-import { Box } from "@mui/material";
+import { Box, createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      paper: "#fff",
+    },
+    text: {
+      primary: "#173A5E",
+      secondary: "#46505A",
+    },
+    action: {
+      active: "#001E3C",
+    },
+    success: {
+      dark: "#009688",
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div>
-      <NestedAccordion
-        title="Parent Accordion 1"
-        content="This is the parent accordion content."
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          bgcolor: "background.paper",
+          boxShadow: 1,
+          borderRadius: 2,
+          p: 2,
+          minWidth: 300,
+          m: 2,
+        }}
       >
-        <Box mt={2}>
-          <NestedAccordion
-            title="Child Accordion 1.1"
-            content="This is the nested accordion content."
-          />
-          <NestedAccordion
-            title="Child Accordion 1.2"
-            content="This is another nested accordion content."
-          />
-        </Box>
-      </NestedAccordion>
+        <NestedAccordion
+          title="Parent Accordion 1"
+          content="This is the parent accordion content."
+        >
+          <Box mt={2}>
+            <NestedAccordion
+              title="Child Accordion 1.1"
+              content="This is the nested accordion content."
+            />
+            <NestedAccordion
+              title="Child Accordion 1.2"
+              content="This is another nested accordion content."
+            />
+          </Box>
+        </NestedAccordion>
 
-      <NestedAccordion
-        title="Parent Accordion 2"
-        content="Another parent accordion content."
-      />
-    </div>
+        <NestedAccordion
+          title="Parent Accordion 2"
+          content="Another parent accordion content."
+        />
+      </Box>
+    </ThemeProvider>
   );
 };
 
